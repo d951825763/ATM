23b043309c06f9811931e5879a2d79e9b00671e1
d951825763 <951825763@qq.com>
2018-01-10 15:57:36 +0800

    ATM模拟程序

---
+++ com/feicuiedu/ATM/Enter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Enter{
+	System.out.println("      欢迎使用逗比ATM");
+
+	System.out.println("请输入您的账号：");
+    String accountinput = new Scanner(System.in).nextLine;
+
+	System.out.println("请输入您的密码：");
+    String passwordinput = new Scanner(System.in).nextLine;
+} 
