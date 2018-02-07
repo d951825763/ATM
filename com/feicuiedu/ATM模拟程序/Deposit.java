b85c2092788bd5fa37dfaacc1b746a1a57edaccc
小蔡啊 <34933114+d951825763@users.noreply.github.com>
2018-02-07 17:25:20 +0800

    Delete Deposit.java

--- com/feicuiedu/ATM模拟程序/Deposit.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Deposit{
-	System.out.println("   存款业务");
-	System.out.println("请输入存款金额：");
-	double moneyinput = new Scanner(System.in).nextDouble;
-	System.out.println(" 1-确认/n 2-重新输入/n 3-返回菜单");
-	String input = new Scanner(System.in).nextLine;
-}
